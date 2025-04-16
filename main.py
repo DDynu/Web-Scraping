1da34bf2c784930b3aa886b4d5160045a32ef420
Ddynu <khoadang826@gmail.com>
2025-04-16 19:52:30 +0700

    change gitignore, changes to main to format everything into a html file

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-import sys
-
 import asyncio
+import os
+import sys
 
 from contentParser import parseContent
 from Scraper import Scraper
 
 
 async def main():
+    # Get the argument (website link)
     WEB_URL = getArgs()
     if WEB_URL:
         print(WEB_URL)
+
+    # Create directory if not exists
+    if not os.path.exists("./parsed/"):
+        os.mkdir("./parsed")
+    if not os.path.exists("./raw/"):
+        os.mkdir("./raw")
+
+    # Remove the parsed file if exists
+    try:
+        os.remove("./parsed/all_div.html")
+    except OSError:
+        pass
 
     # Launch the browser
     scr1 = Scraper()
@@ -17,23 +30,39 @@ async def main():
     await scr1.scrapePage(WEB_URL)
 
     # Put into HTML file
-    with open(f'./parsed/all_div.html', 'a', encoding='utf-8') as htmlFile:
-        htmlFile.write("<div class=\"page\">")
+    htmlStart = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    """
+    htmlEnd = """
+    <script src="../script.js"></script>
+</body>
+</html>
+    """
+    with open(f"./parsed/all_div.html", "a", encoding="utf-8") as htmlFile:
+        htmlFile.write(htmlStart)
 
     for i in range(2, 29):
-        with open(f'./raw/div_content-{i}.html', 'r', encoding='utf-8') as file:
+        with open(f"./raw/div_content-{i}.html", "r", encoding="utf-8") as file:
             html_content = file.read()
-
-        with open(f'./parsed/all_div.html', 'a', encoding='utf-8') as htmlFile:
-            htmlFile.write("<section>")
+            
+        with open(f"./parsed/all_div.html", "a", encoding="utf-8") as htmlFile:
+            htmlFile.write(f'<section class="page">')
             htmlFile.write(html_content)
             htmlFile.write("<br>")
             htmlFile.write("</section>")
 
-    with open(f'./parsed/all_div.html', 'a', encoding='utf-8') as htmlFile:
-        htmlFile.write("</div>")
+    with open(f"./parsed/all_div.html", "a", encoding="utf-8") as htmlFile:
+        htmlFile.write(htmlEnd)
 
-    with open('./parsed/all_div.html', 'r', encoding='utf-8') as htmlFile:
+    with open("./parsed/all_div.html", "r", encoding="utf-8") as htmlFile:
         content = htmlFile.read()
 
     #
@@ -43,6 +72,7 @@ async def main():
     #         file.write(parsed)
     #
     #     print(f'parsed {i}')
+
 
 def getArgs():
     if len(sys.argv) > 2 and sys.argv[1].__eq__("--url"):
